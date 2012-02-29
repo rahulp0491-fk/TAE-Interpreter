ea570bf23a08627abe4894d30e04a93c3cd1af09
Rahul P <rahulp0491@gmail.com>
2012-03-01 02:42:01 +0530

    Added test cases

---
+++ tyarith/test.f
@@ -0,0 +1,11 @@
+/* Examples for testing */
+
+succ 0;
+pred (succ (pred (pred 0)));
+iszero (succ (pred (succ (succ 0))));
+if true then (if true then false else (succ 0)) else (if true then 0 else false);
+succ 2;
+succ -3;
+pred -1;
+pred 2;
+-2;
